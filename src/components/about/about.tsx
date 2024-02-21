a75ddb186e065048ad700dc641b13cbe177ec7d8
RicardoLocher <ricardo.locher123@gmail.com>
2024-02-21 14:37:53 +0100

    Wrote Text about me

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -3,11 +3,21 @@ import './about.css'
 export function About() {
   return (
     <div className='aboutContainer'>
-      <span className="aboutHeading">Über mich</span>
-      <span className="aboutText"><br/>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quam ipsum dicta numquam nesciunt commodi nisi molestias laborum quidem. Inventore veniam minima tenetur iure, fugiat et doloribus earum dolor adipisci!</span>
-      <span className="aboutText"><br/>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quam ipsum dicta numquam nesciunt commodi nisi molestias laborum quidem. Inventore veniam minima tenetur iure, fugiat et doloribus earum dolor adipisci!</span>
-      <span className="aboutText"><br/>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quam ipsum dicta numquam nesciunt commodi nisi molestias laborum quidem. Inventore veniam minima tenetur iure, fugiat et doloribus earum dolor adipisci!</span>
-      <span className="aboutText"><br/>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quam ipsum dicta numquam nesciunt commodi nisi molestias laborum quidem. Inventore veniam minima tenetur iure, fugiat et doloribus earum dolor adipisci!</span>
+      <span className="aboutText"><br/>
+        Ich war schon immer an Computern und alles was mit Technik zu tun hat interessiert,
+        doch richtig angefangen hat es erst, als ich 2017 über mein erstes Github Repository gestolpert bin.
+        Seitdem habe ich mich immer mehr in die Welt der Softwareentwicklung eingearbeitet und bin heute fest davon überzeugt,
+        dass ich in diesem Bereich meine Zukunft sehe. <br/>
+        <br/>
+        Ich habe bereits Erfahrungen in verschiedenen Programmiersprachen und Frameworks gesammelt, 
+        doch am meisten Spaß macht mir die Arbeit mit JavaScript und TypeScript.
+        Ich bin immer auf der Suche nach neuen Herausforderungen und freue mich vor allem darüber,
+        dass ich ein Teil des Technologischen fortschritts sein kann, denn: <br/>
+        <br/>
+      </span>
+      <span className='abutQuote'>Jede hinreichend fortschrittliche Technologie ist von Magie nicht zu unterscheiden.<br/>
+        - Arthur C. Clarke
+      </span>
     </div>
   );
 }
