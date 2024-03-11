e0d25c2db96e260c6d29fa09d32399d28d365f9a
RicardoLocher <ricardo.locher123@gmail.com>
2024-03-11 12:22:29 +0100

    Fixed Bug with overlapping Parallax Containers

--- src/main.tsx
+++ src/main.tsx
@@ -114,11 +114,11 @@ function App() {
             <Certificates />
           </Parallax>
 
-          <Parallax translateY={[-5, 5]} style={{width: '100%', height: '100%', margin: '20px'}} speed={-100}>
+          <Parallax translateY={[-5, 5]} style={{width: '100%', height: '100%', margin: '20px', marginBottom: '200px'}} speed={-100}>
             <TechStack />
           </Parallax>
 
-          <Parallax translateY={[-5, 5]} style={{width: '100%', height: '100%', margin: '20px'}} speed={-10}>
+          <Parallax translateY={[-5, 5]} style={{width: '100%', height: '100%', margin: '20px', marginTop: '100px'}} speed={-10}>
            <Projects />
           </Parallax>
         </div>
