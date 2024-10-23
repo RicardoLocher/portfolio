91aff7d01f60998ef2a62811f78abe43c1f794ca
RicardoLocher <ricardo.locher123@gmail.com>
2024-10-23 12:02:48 +0200

    Updated About Text

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -4,19 +4,24 @@ export function About() {
   return (
     <div className='aboutContainer'>
       <span className="aboutText sec" id='about'>
-        Ich war schon immer an Computern und an allem was mit Technik zu tun hat interessiert,
-        doch richtig angefangen hat es erst, als ich 2017 über mein erstes Github Repository gestolpert bin.
-        Seitdem habe ich mich immer mehr in die Welt der Softwareentwicklung eingearbeitet und bin heute fest davon überzeugt,
-        dass ich in diesem Bereich meine Zukunft sehe. <br/>
-        <br/>
-        Ich habe bereits Erfahrungen in verschiedenen Programmiersprachen und Frameworks gesammelt, 
-        doch am meisten Spaß macht mir die Arbeit mit JavaScript und TypeScript.
-        Ich bin immer auf der Suche nach neuen Herausforderungen und freue mich vor allem darüber,
-        dass ich ein Teil des Technologischen fortschritts sein kann, denn: <br/>
-        <br/>
-      </span>
-      <span className='abutQuote'>Jede hinreichend fortschrittliche Technologie ist von Magie nicht zu unterscheiden.<br/>
-        - Arthur C. Clarke
+        Willkommen auf meiner Website! ich bin leidenschaftlicher Softwareentwickler mit besonderem Fokus 
+        auf Webentwicklung im Front-End als auch im Back-End. In den letzten Jahren habe ich viel Praktische Erfahrung
+        in der Entwicklung spezialisierter Projekte durch mehrere Harvard-Kurse als auch durch meinen Job als Softwareentwickler 
+        gesammelt. 
+        <br/><br/>
+        Mein technisches Know-how erstreckt sich über verschiedene Bereiche, darunter die Implementierung von 
+        Echtzeit-Rendering, optimierter Algorithmen zur Performance-Steigerung sowie die Entwicklung von 
+        Full-Stack Anwendungen und Videospielen. Dabei habe ich stets einen hohen Anspruch an sauberen, 
+        effizienten und gut strukturierten Code, der nicht nur funktioniert, 
+        sondern auch langfristig skalier- und wartbar ist. 
+        <br/><br/>
+        Neben meiner Liebe zur Softwareentwicklung bin ich stets neugierig, 
+        neue Technologien und Werkzeuge zu erkunden.
+        Ich freue mich immer darauf, mich weiterzuentwickeln,
+        neue Herausforderungen anzunehmen und innovative Lösungen zu finden.
+        <br/><br/>
+        Wenn sie mehr über meine Projekte erfahren möchten
+        oder nach einem engagierten Entwickler für ihr nächstes Projekt suchen, zögern sie nicht, mich zu kontaktieren!
       </span>
     </div>
   );
